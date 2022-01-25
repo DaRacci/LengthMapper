13659a1e965155047b0053192b29f882c7e2585d
Racci <90304606+DaRacci@users.noreply.github.com>
2022-01-25 16:53:12 +1100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    kotlin("multiplatform") version "1.6.10"
+    kotlin("plugin.serialization") version "1.6.10"
+}
+
+group = "dev.racci"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+
+    explicitApi()
+
+    targets.all {
+        compilations.all {
+            kotlinOptions {
+                apiVersion = "1.6"
+                languageVersion = "1.6"
+            }
+        }
+    }
+
+    for (target in listOf(linuxX64(), mingwX64())) {
+        target.apply {
+            compilations["main"].enableEndorsedLibs = false
+            binaries {
+                executable {
+                    entryPoint = "main"
+                }
+            }
+        }
+    }
+
+    sourceSets {
+
+        all {
+            languageSettings {
+                optIn("kotlin.time.ExperimentalTime")
+            }
+        }
+
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib"))
+                implementation(kotlin("reflect"))
+                implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.4")
+                implementation("com.soywiz.korlibs.korio:korio:2.4.10")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+                implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.5")
+            }
+        }
+        val linuxX64Main by getting { dependsOn(commonMain) }
+        val mingwX64Main by getting { dependsOn(commonMain) }
+    }
+}
---
+++ src/commonMain/kotlin/Main.kt
@@ -0,0 +1,82 @@
+
+import com.soywiz.kds.fastCastTo
+import com.soywiz.korio.async.async
+import com.soywiz.korio.async.use
+import com.soywiz.korio.file.VfsFile
+import com.soywiz.korio.file.VfsOpenMode
+import com.soywiz.korio.file.std.localVfs
+import com.soywiz.korio.stream.AsyncStream
+import com.soywiz.korio.stream.readAll
+import com.soywiz.korio.stream.writeString
+import kotlinx.cinterop.toKString
+import kotlinx.cli.ArgParser
+import kotlinx.cli.ArgType
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import kotlin.system.exitProcess
+
+public fun main(args: Array<String>) {
+    runBlocking {
+        val parser = ArgParser("LengthMapper")
+
+        val fileName by parser.option(ArgType.String, "file", "f", "Points to the file used in this operation")
+        val output by parser.option(ArgType.String, "output", "o", "Where to output the new file to.")
+        parser.parse(args)
+
+        requireNotNull(fileName) { "You must specify a file using the --file or -f Argument." }
+        val file = localVfs(fileName!!, true)
+        require(file.exists()) { "This file doesn't exist." }
+        require(file.isFile()) { "The selected file must be a text file." }
+
+        val contents = async { getFileContents(file) }
+
+        val newFile: Deferred<AsyncStream> = async {
+            val outputFile = if (output != null) {
+                localVfs(output!!)
+            } else localVfs("${file.parent.path}/output.json")
+
+            var openStream: AsyncStream? = null
+            if (outputFile.exists()) {
+                print("The file ${outputFile.path} already exists, do you want to overwrite it? [y/N]: ")
+                while (openStream == null) {
+                    val response = readLine()
+                    when (response?.lowercase()) {
+                        "y", "yes", "t", "true" -> {
+                            outputFile.delete()
+                            openStream = outputFile.open(VfsOpenMode.CREATE)
+                        }
+                        "n", "no", "f", "false" -> {
+                            exitProcess(0)
+                        }
+                        else -> {
+                            print("Could not parse $response to a boolean, please try again. [y/N]: ")
+                        }
+                    }
+                }
+                openStream
+            } else return@async outputFile.open(VfsOpenMode.CREATE)
+        }
+        val (map, outputFile) = awaitAll(contents, newFile)
+
+        val jsonString = Json.encodeToString<MutableMap<Int, MutableSet<String>>>(map.fastCastTo())
+        (outputFile as AsyncStream).writeString(jsonString)
+
+        println("Finished!")
+    }
+}
+
+public suspend fun getFileContents(file: VfsFile): MutableMap<Int, MutableSet<String>> {
+    val map = mutableMapOf<Int, MutableSet<String>>()
+    for (num in 1..34) {
+        map[num] = mutableSetOf()
+    }
+    file.openInputStream().use {
+        readAll().toKString().splitToSequence('\n').forEach { word ->
+            map[word.length]?.add(word) ?: error("$word was supposedly longer than the generate max length of 34???")
+        }
+    }
+    return map
+}
